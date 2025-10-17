213a0b280d17e583b621867fbed16d0135a18326
kaashishh <kashishbansal002@gmail.com>
2025-10-17 17:35:54 +0530

    add index.html file

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#element", {
+  strings: [
+    "Software Developer",
+    "Web Developer",
+    "Full Stack Developer",
+    "Game Developer",
+    "Node.js Developer",
+    "MongoDB Developer",
+  ],
+  typeSpeed: 40,
+});
